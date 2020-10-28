b10d8287ca2e724c7360de1cb746cf827017ec2f
biradardd <biradardd@gmail.com>
2020-10-28 14:54:29 +0530

    url

--- accounts/views.py
+++ accounts/views.py
@@ -14,52 +14,6 @@ def upload_file(request):
     return render(request, 'accounts/upload_file.html')
 
 
-@login_required
-def record_list(request):
-
-    # Reading uploaded file
-    if request.method == 'POST':
-        x = request.FILES['data']
-        json_data = json.load(x)
-
-        # Validation for the content of the file
-        for data in json_data:
-            if set(data.keys()) != {'userId', 'id', 'title', 'body'}:
-                messages.warning(request, "Data not in proper format")
-                return redirect('/accounts/upload-file/')
-
-        # validating for duplicate records
-        query = JsonData.objects.all()
-        if query.exists():
-            # new_list = [q.id for q in query]
-            id_list = []
-            for q in query:
-                id_list.append(q.id)
-
-            for data in json_data:
-                if not data['id'] in id_list:
-                    obj = JsonData()
-                    obj.userId = data['userId']
-                    obj.id = data['id']
-                    obj.title = data['title']
-                    obj.body = data['body']
-                    obj.save()
-
-        else:
-            for data in json_data:
-                obj = JsonData()
-                obj.userId = data['userId']
-                obj.id = data['id']
-                obj.title = data['title']
-                obj.body = data['body']
-                obj.save()
-
-        messages.info(request, "Data stored successfully")
-
-    # fetch_data = JsonData.objects.all()
-    return redirect('/accounts/record-list2/')
-
-
 class RecordList(LoginRequiredMixin, ListView):
     model = JsonData
     template_name = 'accounts/record_list.html'
--- financepeer/urls.py
+++ financepeer/urls.py
@@ -3,15 +3,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from accounts.views import upload_file, record_list, RecordList
+from accounts.views import upload_file, RecordList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', TemplateView.as_view(template_name="home.html")),
     path('accounts/upload-file/', upload_file, name="upload_file"),
-    path('accounts/record-list/', record_list, name="record_list"),
-    path('accounts/record-list2/', RecordList.as_view(), name="record_list2")
+    path('accounts/record-list/', RecordList.as_view(), name="record_list")
 ]
 
 if settings.DEBUG:
